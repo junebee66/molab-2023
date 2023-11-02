d4aa154f8d15ad0d77966859bad61969a9cb65fd
June Bee <junebee66@gmail.com>
2023-11-02 00:58:41 -0400

    fix ai texture on sphere and upload gifs

--- Week08/ios14ARTest/ios14ARTest/ContentView.swift
+++ Week08/ios14ARTest/ios14ARTest/ContentView.swift
@@ -1,74 +1,76 @@
 import SwiftUI
-import RealityKit
 import ARKit
 import SceneKit
-import UIKit
 
 struct ARKitSceneView: View {
-    @AppStorage("username") var username: String = "Anonymous"
-    @AppStorage("score") var score: Int = 0
-    @State private var lightColor = false
     @State private var promptText = "beautiful landscape"
     @State private var heightText = "360"
     @State private var widthText = "480"
     @State private var isLoading = false
     @State private var image: UIImage?
     @State private var showDownloadButton = false
-    @State private var currentTexture = "mars.jpg" // Initial texture for the sphere
+    @State private var currentTexture = "galaxy.jpg" // Initial texture for the sphere
 
     var body: some View {
         ZStack {
-            ARViewContainer(currentTexture: currentTexture).id(currentTexture)
-
-            VStack {
-                Text("Image Generator")
+            ARViewContainer(currentTexture: $currentTexture).id(currentTexture)
+            
+            HStack{
+                Text("AI Texture Generator")
                     .font(.title)
-
-                TextField("Prompt", text: $promptText)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-
-                HStack {
-                    TextField("Height", text: $heightText)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                    TextField("Width", text: $widthText)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                }
-
-                HStack {
-                    Button(action: generateImage) {
-                        Text("Generate")
+            }
+            HStack{
+                VStack {
+                    
+                    if image != nil {
+                        Image(uiImage: image!)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 300, height: 200)
+                            .padding()
                     }
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                    .frame(width: 100)
                 }
-
-                if isLoading {
-                    ProgressView("Loading...")
-                }
-
-                if image != nil {
-                    Image(uiImage: image!)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 300, height: 200)
-                        .padding()
-                }
-
-                if showDownloadButton {
-                    Button(action: downloadImage) {
-                        Text("Download")
-                    }
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                }
+                .frame(width: 300)
+            }
+        }
+        if isLoading {
+            ProgressView("Loading...")
+        }
+        
+        
+//        if showDownloadButton {
+//            Button(action: downloadImage) {
+//                Text("Update Texture")
+//            }
+//            .padding()
+//            .background(Color.blue)
+//            .foregroundColor(.white)
+//            .cornerRadius(10)
+//        }
+        
+        HStack {
+            TextField("Height", text: $heightText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Width", text: $widthText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
+        
+        TextField("Prompt", text: $promptText)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+        
+        
+        HStack {
+            Button(action: generateImage) {
+                Text("Generate")
             }
             .padding()
+            .background(Color.white)
+            .foregroundColor(.black)
+            .cornerRadius(10)
+            .frame(width: 800)
+            
         }
+
     }
 
     func generateImage() {
@@ -115,21 +117,14 @@ struct ARKitSceneView: View {
         }
     }
         
-        
     func downloadImage() {
         // Implement the download functionality here, e.g., using UIActivityViewController or other methods.
-    }
-}
-
-class LightingRed: Entity, HasPointLight {
-    required init() {
-        super.init()
-        self.light = PointLightComponent(color: .red, intensity: 100000, attenuationRadius: 20)
+        currentTexture = "generatedImage.jpg"
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
-    var currentTexture: String
+    @Binding var currentTexture: String  // Use a binding for the current texture
 
     func makeUIView(context: Context) -> ARSCNView {
         let sceneView = ARSCNView(frame: .zero)
@@ -169,7 +164,15 @@ struct ARViewContainer: UIViewRepresentable {
             // Create a new sphere with the current texture
             let sphere = SCNSphere(radius: 0.2)
             let material = SCNMaterial()
-            material.diffuse.contents = UIImage(named: parent.currentTexture)
+            
+            // Load the image from the app's Documents directory
+            if let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                let fileURL = documentsDirectory.appendingPathComponent(parent.currentTexture)
+                if let image = UIImage(contentsOfFile: fileURL.path) {
+                    material.diffuse.contents = image
+                }
+            }
+            
             sphere.materials = [material]
 
             let sphereNode = SCNNode(geometry: sphere)
@@ -178,10 +181,3 @@ struct ARViewContainer: UIViewRepresentable {
         }
     }
 }
-#if DEBUG
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ARKitSceneView()
-    }
-}
-#endif
---
+++ Week08/ios14ARTest/ios14ARTest/DepthMap.swift
@@ -0,0 +1,8 @@
+//
+//  DepthMap.swift
+//  ios14ARTest
+//
+//  Created by June Bee on 11/1/23.
+//
+
+import Foundation
